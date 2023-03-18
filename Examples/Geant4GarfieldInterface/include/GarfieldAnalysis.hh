6d392f1fd709d33e45446aa799c191f57a6ba167
Heinrich Schindler <hschindl@lxplus718.cern.ch>
2023-03-18 12:01:04 +0100

    Make G4-Garfield example compatible with Geant4 11. Thanks to Jean Michel Friedt for the fix.

--- Examples/Geant4GarfieldInterface/include/GarfieldAnalysis.hh
+++ Examples/Geant4GarfieldInterface/include/GarfieldAnalysis.hh
@@ -28,10 +28,16 @@
 /// \file GarfieldAnalysis.hh
 /// \brief Selection of the analysis technology
 
-#ifndef Garfieldnalysis_h
-#define Garfieldnalysis_h 1
+#ifndef GarfieldAnalysis_h
+#define GarfieldAnalysis_h 1
 
-#include "g4root.hh"
-//#include "g4xml.hh"
+#include "G4Version.hh"
+
+#if (G4VERSION_NUMBER < 1100)
+  #include "g4root.hh"
+  //#include "g4xml.hh"
+#else
+  #include "G4AnalysisManager.hh"
+#endif 
 
 #endif
