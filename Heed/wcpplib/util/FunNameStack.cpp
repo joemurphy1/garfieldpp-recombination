bd21533bfb274f7acb767ca2bb23fbfd49be60c1
flagarde <flagarde.programs@pm.me>
2025-06-04 00:26:55 +0800

    clean

--- Heed/wcpplib/util/FunNameStack.cpp
+++ Heed/wcpplib/util/FunNameStack.cpp
@@ -10,32 +10,17 @@ The file is provided "as is" without express or implied warranty.
 
 #include "wcpplib/util/FunNameStack.h"
 
-#include <cstring>
-#include <iomanip>
-#include <iostream>
+#include <cstdlib>
 
 namespace Heed {
-
-int s_throw_exception_in_spexit = 0;
-int s_exit_without_core = 0;
 
 void spexit_action(std::ostream& file) {
   file << "spexit_action: the streams will be now flushed\n";
   file.flush();
   mcout.flush();
   mcerr.flush();
-  if (s_throw_exception_in_spexit != 1) {
-    if (s_exit_without_core == 1) {
-      file << "spexit_action: the exit(1) function is called\n";
-      exit(1);
-    } else {
-      file << "spexit_action: the abort function is called\n";
-      abort();
-    }
-  } else {
-    file << "spexit_action: an exception is now called\n";
-    throw ExcFromSpexit();
-  }
+  file << "spexit_action: the exit(1) function is called\n";
+  std::exit(1);
 }
 
 }  // namespace Heed
--- Heed/wcpplib/util/FunNameStack.h
+++ Heed/wcpplib/util/FunNameStack.h
@@ -11,27 +11,9 @@ and notices about any modifications of the original text
 appear in all copies and in supporting documentation.
 It is provided "as is" without express or implied warranty.
 */
-
-#include <cstdlib>
-#include <iostream>
-#include <list>
+#include <ostream>
 
 #include "wcpplib/stream/prstream.h"
-
-// #define USE_BOOST_MULTITHREADING
-// #define PRINT_MESSAGE_ABOUT_THREAD_INITIALIZATION
-//  Prints a long message at initialization of FunNameStack.
-//  The message is quite long and it is unlikely that
-//  it can be visually missed even if it is interrupted by play of threads.
-//  This message is not necessary in routine work.
-
-/*
-// Here there is a good place to switch off the
-// initialization of the function names in all the programs
-#ifdef FUNNAMESTACK
-#undef FUNNAMESTACK
-#endif
-*/
 
 // Switch on/off initialization of function names.
 #define mfunname(string)
@@ -118,15 +100,7 @@ It is provided "as is" without express or implied warranty.
 
 namespace Heed {
 
-class ExcFromSpexit {
- public:
-  ExcFromSpexit() {}
-};
-
 void spexit_action(std::ostream& file);
-extern int s_throw_exception_in_spexit;  // if == 1, does exit(1) of abort()
-                                         // depending on the key below
-extern int s_exit_without_core;          // the key above have larger priority
 
 }  // namespace Heed
 
