c5ed63600d9981d06ac6bb7085d8ff4509a7ed40
flagarde <flagarde.programs@pm.me>
2025-09-04 20:30:55 +0800

    remove macro

--- GPU/MediumGPU.h
+++ GPU/MediumGPU.h
@@ -1,10 +1,6 @@
 #ifndef G_MEDIUMGPU_H
 #define G_MEDIUMGPU_H
 
-#ifndef __GPUCOMPILE__
-#error GPU HEADER INCLUDED WITHOUT SETTING __GPUCOMPILE__
-#endif
-
 #include "GPUInterface.hh"
 #include "Garfield/MagboltzInterface.hh"
 
